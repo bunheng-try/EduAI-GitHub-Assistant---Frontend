53d7dc293d9c6d44d34b92b1bc601195df155e3d
leap170505 <leap170505@gmail.com>
2026-01-14 16:31:16 +0700

    BX-49

--- src/shared/components/appShell/MainBar.tsx
+++ src/shared/components/appShell/MainBar.tsx
@@ -1,11 +1,9 @@
-import AssignmentList from '@/features/assignment/components/AssignmentsList'
-import React from 'react'
+import { ClassroomMainBar } from '@/features/class/pages/ClassroomMainBar'
 
 const MainBar = () => {
   return (
     <div className='flex flex-col w-full h-screen pt-4 bg-blue-950 text-white px-4'>
-      MainBar
-      <AssignmentList />
+      <ClassroomMainBar/>
     </div>
   )
 }
