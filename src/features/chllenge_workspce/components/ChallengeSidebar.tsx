444a7a8b788519ae9e31933eab8d2189b4d2bd98
leang-dot <ounleang180@gmail.com>
2026-03-15 23:39:40 +0700

    COD-92 Challenge Navigation (Left Sidebar)

--- src/features/chllenge_workspce/components/ChallengeSidebar.tsx
+++ src/features/chllenge_workspce/components/ChallengeSidebar.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import type { Challenge } from "@/shared/types/types";
+import React, { useEffect } from "react";
+import { ChallengeItem } from "./CHallengeItem";
+import { useWorkspaceStore } from "../stores/useWorkspaceStore";
 
-const ChallengeSidebar = () => {
-  return (
-    <div className='bg-red-500'>ChallengeSidebar</div>
-  )
+interface ChallengeSidebarProps {
+  challenges: Challenge[];
 }
 
-export default ChallengeSidebar
+const ChallengeSidebar: React.FC<ChallengeSidebarProps> = ({ challenges }) => {
+  const { currentChallengeId, setCurrentChallenge } = useWorkspaceStore();
+
+  return (
+    <div
+      className="
+        flex flex-col items-center
+        w-16
+        h-full
+        bg-[hsl(var(--workspace))] 
+        border-r border-[hsl(var(--border-strong))] 
+        p-2 
+        gap-2
+        overflow-y-auto
+        scrollbar-thin scrollbar-thumb-[hsl(var(--border))] scrollbar-track-[hsl(var(--workspace))]
+      "
+    >
+      {challenges.map((c, index) => (
+        <ChallengeItem
+          key={c.id}
+          name={(index + 1).toString()}
+          active={currentChallengeId === Number(c.id)}
+          onClick={() => setCurrentChallenge(Number(c.id))}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ChallengeSidebar;
