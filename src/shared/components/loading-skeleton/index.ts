1ad71351aa54a36885352e44133418bb103988d2
leang-dot <ounleang180@gmail.com>
2026-03-20 16:18:04 +0700

    COD-103

--- src/shared/components/loading-skeleton/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as SkeletonBox } from "./SkeletonBox";
-export { default as HeaderSkeleton } from "./HeaderSkeleton";
-export { default as PanelSkeleton } from "./PanelSkeleton";
-export { default as CardListSkeleton } from "./CardListSkeleton";
-export { default as SidebarSkeleton } from "./SidebarSkeleton";
-export { default as FormSkeleton } from "./FormSkeleton";
-export { default as WorkspaceSkeleton } from "./WorkspaceSkeleton";
